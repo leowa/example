32015c903fafade8dbc31ae442e556c727901d0d
Andy Zhang <zch.ustc@gmail.com>
2018-08-20 13:25:51 +0800

    add bash script example

---
+++ bash.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+set -o errexit
+set -o pipefail
+set -o nounset
+# set -o xtrace
+
+# Set magic variables for current file & dir
+__dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+__file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
+__base="$(basename ${__file} .sh)"
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
+
+# get the first arg
+arg1="${1:-}"
